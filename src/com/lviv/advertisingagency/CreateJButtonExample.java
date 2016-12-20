b10d8c58b8e6f480df7b27820577ad7384597ad0
VitaliiJava <superpills@mail.ru>
2016-12-20 22:20:36 +0200

    new commit app create

--- src/com/lviv/advertisingagency/CreateJButtonExample.java
+++ src/com/lviv/advertisingagency/CreateJButtonExample.java
@@ -1,11 +1,6 @@
 package com.lviv.advertisingagency;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-
-public class CreateJButtonExample{
+public class CreateJButtonExample {
 
 }
